e93b7bdada600dc720b9a394548dd2256ab567e8
Jairjmma <jairjmma@gmail.com>
2025-01-31 15:38:39 +0300

    v4-cju.sql

--- v4-cju.sql
+++ v4-cju.sql
@@ -1,3 +1,18 @@
-/**
-  @author
- */
+WITH Productos_Caros AS (
+    SELECT codigo_producto, nombre_producto, precio
+    FROM Productos
+    WHERE precio > 20
+)
+SELECT * FROM Productos_Caros;
+
+
+SELECT p.id_pedido, c.nombre_cliente, pr.nombre_producto, dp.cantidad
+FROM Pedidos p
+         JOIN Clientes c ON p.email_cliente = c.email
+         JOIN Detalles_Pedido dp ON p.id_pedido = dp.id_pedido
+         JOIN Productos pr ON dp.codigo_producto = pr.codigo_producto;
+
+
+SELECT nombre_cliente AS nombre FROM Clientes
+UNION
+SELECT nombre_producto AS nombre FROM Productos;
